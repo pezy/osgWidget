96c83a52085babc19fe9e4e3b7627d233bea8713
chenpeizhe <peizhe.chen@horizon.ai>
2020-11-14 20:19:18 +0800

    feat[example]: add colored quad

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,8 +1,9 @@
 #include "mainwindow.h"
 
-#include <osgDB/ReadFile>
-#include <osg/ShapeDrawable>
 #include <osg/Geode>
+#include <osg/Geometry>
+#include <osg/ShapeDrawable>
+#include <osgDB/ReadFile>
 
 #include "osg_widget.h"
 #include "ui_mainwindow.h"
@@ -35,6 +36,31 @@ MainWindow::MainWindow(QWidget *parent)
     root->addDrawable(sphere);
     root->addDrawable(cone);
     osg_widget_->setSceneData(root);
+  });
+
+  connect(ui->actionColored_quad, &QAction::triggered, [this]() {
+      osg::ref_ptr<osg::Vec3Array> vertices = new osg::Vec3Array(4);
+      (*vertices)[0].set(0.f, 0.f, 0.f);
+      (*vertices)[1].set(1.f, 0.f, 0.f);
+      (*vertices)[2].set(1.f, 0.f, 1.f);
+      (*vertices)[3].set(0.f, 0.f, 1.f);
+      osg::ref_ptr<osg::Vec3Array> normals = new osg::Vec3Array(1);
+      (*normals)[0].set(0.f, -1.f, 0.f);
+      osg::ref_ptr<osg::Vec4Array> colors = new osg::Vec4Array(4);
+      (*colors)[0].set(1.f, 0.f, 0.f, 1.f);
+      (*colors)[1].set(0.f, 1.f, 0.f, 1.f);
+      (*colors)[2].set(0.f, 0.f, 1.f, 1.f);
+      (*colors)[3].set(1.f, 1.f, 0.f, 1.f);
+      osg::ref_ptr<osg::Geometry> quad = new osg::Geometry;
+      quad->setVertexArray(vertices);
+      quad->setNormalArray(normals);
+      quad->setNormalBinding(osg::Geometry::BIND_OVERALL);
+      quad->setColorArray(colors);
+      quad->setColorBinding(osg::Geometry::BIND_PER_VERTEX);
+      quad->addPrimitiveSet(new osg::DrawArrays(GL_QUADS, 0, 4));
+      osg::ref_ptr<osg::Geode> root = new osg::Geode;
+      root->addDrawable(quad);
+      osg_widget_->setSceneData(root);
   });
 
   ui->actionDefault->trigger();
