b356651503c554f03ff909d4f7ed294cef8652db
chenpeizhe <peizhe.chen@horizon.ai>
2020-11-22 08:29:46 +0800

    add octahedron
    why use triangle strip, the light source have changed?

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -4,6 +4,7 @@
 #include <osg/Geometry>
 #include <osg/ShapeDrawable>
 #include <osgDB/ReadFile>
+#include <osgUtil/SmoothingVisitor>
 
 #include "osg_widget.h"
 #include "ui_mainwindow.h"
@@ -39,28 +40,52 @@ MainWindow::MainWindow(QWidget *parent)
   });
 
   connect(ui->actionColored_quad, &QAction::triggered, [this]() {
-      osg::ref_ptr<osg::Vec3Array> vertices = new osg::Vec3Array(4);
-      (*vertices)[0].set(0.f, 0.f, 0.f);
-      (*vertices)[1].set(1.f, 0.f, 0.f);
-      (*vertices)[2].set(1.f, 0.f, 1.f);
-      (*vertices)[3].set(0.f, 0.f, 1.f);
-      osg::ref_ptr<osg::Vec3Array> normals = new osg::Vec3Array(1);
-      (*normals)[0].set(0.f, -1.f, 0.f);
-      osg::ref_ptr<osg::Vec4Array> colors = new osg::Vec4Array(4);
-      (*colors)[0].set(1.f, 0.f, 0.f, 1.f);
-      (*colors)[1].set(0.f, 1.f, 0.f, 1.f);
-      (*colors)[2].set(0.f, 0.f, 1.f, 1.f);
-      (*colors)[3].set(1.f, 1.f, 0.f, 1.f);
-      osg::ref_ptr<osg::Geometry> quad = new osg::Geometry;
-      quad->setVertexArray(vertices);
-      quad->setNormalArray(normals);
-      quad->setNormalBinding(osg::Geometry::BIND_OVERALL);
-      quad->setColorArray(colors);
-      quad->setColorBinding(osg::Geometry::BIND_PER_VERTEX);
-      quad->addPrimitiveSet(new osg::DrawArrays(GL_QUADS, 0, 4));
-      osg::ref_ptr<osg::Geode> root = new osg::Geode;
-      root->addDrawable(quad);
-      osg_widget_->setSceneData(root);
+    osg::ref_ptr<osg::Vec3Array> vertices = new osg::Vec3Array(4);
+    (*vertices)[0].set(0.f, 0.f, 0.f);
+    (*vertices)[1].set(1.f, 0.f, 0.f);
+    (*vertices)[2].set(1.f, 0.f, 1.f);
+    (*vertices)[3].set(0.f, 0.f, 1.f);
+    osg::ref_ptr<osg::Vec3Array> normals = new osg::Vec3Array(1);
+    (*normals)[0].set(0.f, -1.f, 0.f);
+    osg::ref_ptr<osg::Vec4Array> colors = new osg::Vec4Array(4);
+    (*colors)[0].set(1.f, 0.f, 0.f, 1.f);
+    (*colors)[1].set(0.f, 1.f, 0.f, 1.f);
+    (*colors)[2].set(0.f, 0.f, 1.f, 1.f);
+    (*colors)[3].set(1.f, 1.f, 0.f, 1.f);
+    osg::ref_ptr<osg::Geometry> quad = new osg::Geometry;
+    quad->setVertexArray(vertices);
+    quad->setNormalArray(normals);
+    quad->setNormalBinding(osg::Geometry::BIND_OVERALL);
+    quad->setColorArray(colors);
+    quad->setColorBinding(osg::Geometry::BIND_PER_VERTEX);
+    quad->addPrimitiveSet(new osg::DrawArrays(GL_QUADS, 0, 4));
+    osg::ref_ptr<osg::Geode> root = new osg::Geode;
+    root->addDrawable(quad);
+    osg_widget_->setSceneData(root);
+  });
+
+  connect(ui->actionOctahedron, &QAction::triggered, [this]() {
+    osg::ref_ptr<osg::Vec3Array> vertices = new osg::Vec3Array(6);
+    (*vertices)[0].set(0.f, 0.f, 1.f);
+    (*vertices)[1].set(0.5f, -0.5f, 0.f);
+    (*vertices)[2].set(0.5f, 0.5f, 0.f);
+    (*vertices)[3].set(-0.5f, 0.5f, 0.f);
+    (*vertices)[4].set(-0.5f, -0.5f, 0.f);
+    (*vertices)[5].set(0.f, 0.f, -1.f);
+    // const auto order = {3, 5, 4, 1, 5, 2, 3, 0, 4, 1};
+    // osg::ref_ptr<osg::DrawElementsUInt> indices =
+    //     new osg::DrawElementsUInt(GL_TRIANGLE_STRIP, order.begin(), order.end());
+    const auto order = {0, 1, 2, 0, 2, 3, 0, 3, 4, 0, 4, 1,
+                        5, 2, 1, 5, 3, 2, 5, 4, 3, 5, 1, 4};
+    osg::ref_ptr<osg::DrawElementsUInt> indices =
+        new osg::DrawElementsUInt(GL_TRIANGLES, order.begin(), order.end());
+    osg::ref_ptr<osg::Geometry> geom = new osg::Geometry;
+    geom->setVertexArray(vertices);
+    geom->addPrimitiveSet(indices);
+    osgUtil::SmoothingVisitor::smooth(*geom);
+    osg::ref_ptr<osg::Geode> root = new osg::Geode;
+    root->addDrawable(geom);
+    osg_widget_->setSceneData(root);
   });
 
   ui->actionDefault->trigger();
